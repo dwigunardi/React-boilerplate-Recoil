93eae2c47d5548de6c5426648a775cf690bb981c
dwigunardi <dwigunardi@gmail.com>
2023-03-05 03:58:03 +0700

    initial commit

--- src/routes/mainRoute/index.jsx
+++ src/routes/mainRoute/index.jsx
@@ -6,19 +6,20 @@ import MainLayout from "../../components/layout/MainLayout";
 import Home from "../../pages/Home";
 import LoginPage from "../../pages/Login";
 function MainRoute() {
-  return ( <Suspense
-    fallback={
-      <div style={{ textAlign: "center", fontSize: 56, color: "blue" }}>
-        Loading....
-      </div>
-    }>
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-      </Route>
-      <Route path="/login" element={<LoginPage />} />
-    
-    </Routes>
+  return (
+    <Suspense
+      fallback={
+        <div style={{ textAlign: "center", fontSize: 56, color: "blue" }}>
+          Loading....
+        </div>
+      }
+    >
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </Suspense>
   );
 }
